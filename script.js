e5fadb495b37eca5df5af99017d8ab40644f3a5a
segem001 <kipdom001@gmail.com>
2021-11-29 08:53:53 +0300

    Add a prototype function

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ function CrustType(name, size, top) {
     this.CrustName = name
     this.PizzaSize = size
     this.TopSize = top
+} //prototype
+CrustType.prototype.toppings = function() {
+    return this.TopSize
 }
